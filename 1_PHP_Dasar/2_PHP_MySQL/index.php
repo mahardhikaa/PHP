5e579a2fdd6d4fa7e72ae507121f0ab9cadbfc78
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-08-29 17:48:34 +0700

    create table data

---
+++ 1_PHP_Dasar/2_PHP_MySQL/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>PHP & MySQL</title>
+    <style>
+        img {
+            width: 75px;
+            height: 75px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <table border=1 cellspacing=0 cellpadding=10>
+        <tr>
+            <th>No.</th>
+            <th>Gambar</th>
+            <th>Nama</th>
+            <th>NIM</th>
+            <th>Jurusan</th>
+            <th>Aksi</th>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td><img src="img/gambar1.png"></td>
+            <td>Hafizd Mahardhika</td>
+            <td>185090800111010</td>
+            <td>Fisika</td>
+            <td>
+                <a href="">edit</a>
+                <a href="">delete</a>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
