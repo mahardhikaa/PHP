310ccfebcc6a696593f6753b7a166e7c74ca0a56
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-08-29 14:57:49 +0700

    create admin page

---
+++ 1_PHP_Dasar/1_Get_and_Post/admin.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Admin</title>
+</head>
+<body>
+    <h1>Selamat datang admin</h1>
+    <p><a href="latihan3.php">Logout</a></p>
+</body>
+</html>
