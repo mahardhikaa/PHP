c3f7293b2df1f2068924820a9ccb8dd1bb7bddf9
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-09-01 18:58:33 +0700

    live search with jquery

--- 1_PHP_Dasar/13_Live_Search_jQuery/js/script.js
+++ 1_PHP_Dasar/13_Live_Search_jQuery/js/script.js
@@ -1,16 +1,6 @@
-var keyword = document.getElementById('keyword');
-var button = document.getElementById('submit_search');
-var container = document.getElementById('container');
-
-keyword.addEventListener('keyup', function() {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-        if(xhr.readyState==4 && xhr.status==200){
-            container.innerHTML = xhr.responseText;
-        }
-    };
-
-    xhr.open('GET', 'ajax/mahasiswa.php?key='+keyword.value, true);
-    xhr.send();
+$(document).ready(function(){
+    $('#keyword').on('keyup', function(){
+        $('#container').load('ajax/mahasiswa.php?key='+$('#keyword').val());
+        $('#search_clear').show();
+    });
 });
