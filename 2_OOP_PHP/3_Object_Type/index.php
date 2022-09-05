52feccffcf136c6293a25fd5b4f7bddc076792e2
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-09-05 20:50:42 +0700

    learn object type

--- 2_OOP_PHP/3_Object_Type/index.php
+++ 2_OOP_PHP/3_Object_Type/index.php
@@ -22,11 +22,20 @@ class Produk {
     }
 }
 
+class infoProduk {
+    public function cetak(Produk $produk) {
+        $str = "{$produk->judul} | {$produk->penulis}, {$produk->tahun} ({$produk->pembaca} Orang)";
+        return $str;
+    }
+}
+
 //Object
 $komik = new Produk("One Piece", "Masashi Kishimoto", 1999, 200000);
 $game = new Produk("Auto Chess", "Super Cell", 2014, 1000000);
 $novel = new Produk("Bumi", "Tere Liye");
 $buku = new Produk();
+
+$cetakInfoProduk = new infoProduk(); //Ambil info produk dengan class produk
 
 echo "Komik: ". $komik->getProperty();
 echo "<br>";
@@ -35,4 +44,16 @@ echo "<br>";
 echo "Novel: " . $novel->getProperty();
 echo "<br>";
 echo "Buku: " . $buku->getProperty();
+
+echo "<br>";
+echo "<br>";
+
+//Cetak class dengan class
+echo $cetakInfoProduk->cetak($komik);
+echo "<br>";
+echo $cetakInfoProduk->cetak($game);
+echo "<br>";
+echo $cetakInfoProduk->cetak($novel);
+echo "<br>";
+echo $cetakInfoProduk->cetak($buku);
 ?>
