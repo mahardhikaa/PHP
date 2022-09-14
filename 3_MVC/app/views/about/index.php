54f1a81829da4b160a0a92021e208d7baa3c5e44
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-09-14 19:27:53 +0700

    add image at about page

--- 3_MVC/app/views/about/index.php
+++ 3_MVC/app/views/about/index.php
@@ -1,2 +1,3 @@
 <h1>About Me</h1>
+<img src="<?= BASEURL; ?>/img/gambar1.png" alt="Hafizd" width=120>
 <p>Nama saya <?= $data['nama']?>, Saya adalah seorang <?= $data['pekerjaan']?> dengan umur <?= $data['umur']?></p>
