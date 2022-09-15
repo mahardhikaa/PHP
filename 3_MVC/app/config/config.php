537e291cc1646aa26ba2d43a14e8bc34739e962d
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-09-15 15:39:53 +0700

    create db host, name, pass, user, and base url

---
+++ 3_MVC/app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+const BASEURL = "http://localhost/PHP/3_MVC/public";
+
+#database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpdasar');
+
+?>
