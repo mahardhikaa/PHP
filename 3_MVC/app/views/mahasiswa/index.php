99c37cf66b6e175606e1f6508b46d500dfe512f0
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-09-17 20:27:33 +0700

    add modal in mahasiswa list page

--- 3_MVC/app/views/mahasiswa/index.php
+++ 3_MVC/app/views/mahasiswa/index.php
@@ -1,5 +1,6 @@
 <div class="container">
-<h3>Daftar Mahasiswa</h3>
+<button type="button" class="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target="#tambahData">Tambah data Mahasiswa</button>
+<h3 class="mt-3">Daftar Mahasiswa</h3>
 <table class="table table-success table-striped">
     <thead>
         <th>
@@ -23,4 +24,37 @@
     <?php endforeach ?>
     </tbody>
 </table>
+</div>
+
+
+<!-- Modal -->
+<div class="modal fade" id="tambahData" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="POST">
+          <div class="mb-3">
+            <label for="Nama" class="form-label">Nama</label>
+            <input type="text" class="form-control" id="Nama" name="Nama">
+          </div>
+          <div class="mb-3">
+            <label for="NIM" class="form-label">NIM</label>
+            <input type="number" class="form-control" id="NIM" name="NIM">
+          </div>
+          <div class="mb-3">
+            <label for="Jurusan" class="form-label">Jurusan</label>
+            <input type="text" class="form-control" id="Jurusan" name="Jurusan">
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
+      </div>
+    </div>
+  </div>
 </div>
