865b5510458f7c8db4add0137778b2ac1790a192
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-09-20 17:59:19 +0700

    change title and button modal

---
+++ 3_MVC/public/js/script.js
@@ -0,0 +1,6 @@
+$(function(){
+    $('.ubahData').on('click', function(){
+        $('#modalLabel').html('Ubah Data Mahasiswa');
+        $('.modal-footer button[type=submit]').html('Ubah Data');
+    });
+});
