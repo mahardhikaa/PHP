f6c71d96ecf2bd86491e76223a74bf590bcd2746
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-09-20 19:03:49 +0700

    add ajax to get value database

--- 3_MVC/public/js/script.js
+++ 3_MVC/public/js/script.js
@@ -7,7 +7,23 @@ $(function(){
     $('.ubahData').on('click', function(){
         $('#modalLabel').html('Ubah Data Mahasiswa');
         $('.modal-footer button[type=submit]').html('Ubah Data');
-    });
 
-    
+        $('.modal-dialog form').attr('action', 'http://localhost/PHP/3_MVC/public/mahasiswa/ubah');
+        
+        const id = $(this).data('id');
+
+        $.ajax({
+            url: 'http://localhost/PHP/3_MVC/public/mahasiswa/getUbah',
+            data: {id : id},
+            method: 'post',
+            dataType: 'json',
+            success: function(data){
+                $('#Nama').val(data.Nama);
+                $('#NIM').val(data.NIM);
+                $('#Jurusan').val(data.Jurusan);
+                $('#id').val(data.id);
+                $('#Gambar').val(data.Gambar);
+            }
+        });
+    });
 });
