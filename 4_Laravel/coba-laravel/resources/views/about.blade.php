740b0b4d554c36f9ef892c2f56ab11255a019f90
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-09-23 21:33:40 +0700

    add some information like name email and image

--- 4_Laravel/coba-laravel/resources/views/about.blade.php
+++ 4_Laravel/coba-laravel/resources/views/about.blade.php
@@ -1,10 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>About</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <h3>About Page</h3>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+    <h2>{{ $nama }}</h2>
+    <h6>{{ $email }}</h6>
+    <h6>{{ $alamat }}</h6>
+    <img src="img/gambar3.png" width=150>
+@endsection
