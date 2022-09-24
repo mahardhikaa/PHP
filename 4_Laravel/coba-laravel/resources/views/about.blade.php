7f93c751d7fff8a0a0c84d282d053f46d68d9858
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-09-24 10:51:08 +0700

    change image to circle and set as thumbnail

--- 4_Laravel/coba-laravel/resources/views/about.blade.php
+++ 4_Laravel/coba-laravel/resources/views/about.blade.php
@@ -4,5 +4,5 @@
     <h2>{{ $nama }}</h2>
     <h6>{{ $email }}</h6>
     <h6>{{ $alamat }}</h6>
-    <img src="img/gambar3.png" width=150>
+    <img src="img/gambar3.png" class="img-thumbnail rounded-circle" width=150>
 @endsection
