2de42108575ef3e67df3a68dab5a5dd5b3ce8bf7
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-09-25 20:04:15 +0700

    change notasi array assoc to object

--- 4_Laravel/coba-laravel/resources/views/post.blade.php
+++ 4_Laravel/coba-laravel/resources/views/post.blade.php
@@ -2,9 +2,9 @@
 
 @section('container')
     <article>
-        <h4>{{ $post["title"] }}</h4>
-        <h5>By: {{ $post["by"] }}</h5>
-        <p>{{ $post["body"] }}</p>
+        <h4>{{ $post->title }}</h4>
+        <h5>By: Hafidz M.</h5>
+        <p>{{ $post->body }}</p>
     </article>
 
 <a href="/blog">Back to blog</a>
--- 4_Laravel/coba-laravel/resources/views/posts.blade.php
+++ 4_Laravel/coba-laravel/resources/views/posts.blade.php
@@ -4,12 +4,12 @@
     @foreach($posts as $post)
         <article>
             <h4>
-                <a href="/posts/{{ $post["id"] }}">
-                    {{ $post["title"] }}        
+                <a href="/posts/{{ $post->id }}">
+                    {{ $post->title }}        
                 </a>
             </h4>
-            <h5>By: {{ $post["by"] }}</h5>
-            <p>{{ $post["excerpt"] }}</p>
+            <h5>By: Hafizd M.</h5>
+            <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
 @endsection
