4ac14761af9628bd04fa03a2005f8e4a8a689d13
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-09-27 21:09:10 +0700

    add link author

--- 4_Laravel/coba-laravel/resources/views/posts.blade.php
+++ 4_Laravel/coba-laravel/resources/views/posts.blade.php
@@ -4,14 +4,18 @@
     <h3>Blog Post</h3>
 
     @foreach($posts as $post)
-        <article>
+        <article class="border-bottom mb-4">
             <h4>
                 <a href="/posts/{{ $post->slug }}">
                     {{ $post->title }}        
                 </a>
             </h4>
-            <h5>By: Hafizd M.</h5>
+            <h5>
+                <a href="#">By: {{ $post->user->name }}</a>
+            </h5>
+            <p>Category: <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
             <p>{{ $post->excerpt }}</p>
+            <p><a href="/posts/{{ $post->slug }}">Read More</a></p>
         </article>
     @endforeach
 @endsection
