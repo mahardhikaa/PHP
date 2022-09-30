248ea09441b10e2154a9b7c694dd8374dfb5170e
mahardhikaa <hafidz.mahardika236@gmail.com>
2022-09-30 22:07:12 +0700

    change method name from user to authors

--- 4_Laravel/coba-laravel/app/Models/Post.php
+++ 4_Laravel/coba-laravel/app/Models/Post.php
@@ -15,7 +15,7 @@ class Post extends Model
         return $this->belongsTo(Category::class);
     }
 
-    public function user(){
-        return $this->belongsTo(User::class);
+    public function authors(){
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
